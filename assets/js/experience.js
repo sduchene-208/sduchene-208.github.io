fc768a239a8c4e5185c417a7c35d6d7036e0c6b0
Steven D <78009471+sduchene-208@users.noreply.github.com>
2023-08-16 06:42:39 -0600

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,11 +5,11 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Business Analyst",
+    cardImage: "assets/images/experience-page/GeoSol-Logo_Blue_No URL.jpg",
+    place: "GeoGraphic Solutions, Inc.",
+    time: "(2022 - present)",
+    desp: "<li>Text to be updated.</li>",
   },
   {
     title: "Student Developer",
